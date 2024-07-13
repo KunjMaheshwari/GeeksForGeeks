c57c687f25fdeec22119b4c67902ee13c0604664
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2024-07-13 18:50:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge K sorted linked lists/merge-k-sorted-linked-lists.java
@@ -0,0 +1,120 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+
+class GfG
+{
+    public static void printList(Node node)
+    {
+        while(node != null)
+        {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+    
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {   
+            int N = sc.nextInt();
+            
+            Node []a = new Node[N];
+            
+            for(int i = 0; i < N; i++)
+            {
+                int n = sc.nextInt();
+                
+                Node head = new Node(sc.nextInt());
+                Node tail = head;
+                
+                for(int j=0; j<n-1; j++)
+                {
+                    tail.next = new Node(sc.nextInt());
+                    tail = tail.next;
+                }
+                
+                a[i] = head;
+            }
+            
+            Solution g = new Solution();
+             
+            Node res = g.mergeKList(a,N);
+            if(res!=null)
+                printList(res);
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*class Node
+{
+    int data;
+    Node next;
+    
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+*/
+
+// a is an array of Nodes of the heads of linked lists
+// and N is size of array a
+
+
+class Solution
+{
+    Node mergeSort(Node head1, Node head2){
+        Node dummyNode = new Node(-1);
+        Node temp = dummyNode;
+        
+        
+        while(head1 != null && head2 != null){
+            if(head1.data <= head2.data){
+                temp.next = head1;
+                head1 = head1.next;
+            }else{
+                temp.next = head2;
+                head2 = head2.next;
+            }
+            temp = temp.next;
+            
+            if(head1 != null){
+                temp.next = head1;
+            }
+            
+            if(head2 != null){
+                temp.next = head2;
+            }
+        }
+        return dummyNode.next;
+    }
+    //Function to merge K sorted linked list.
+    Node mergeKList(Node[]arr,int K)
+    {
+        //Add your code here.
+        Node head = arr[0];
+        for(int i=1;i<K;i++){
+            head = mergeSort(head, arr[i]);
+        }
+        return head;
+    }
+}
