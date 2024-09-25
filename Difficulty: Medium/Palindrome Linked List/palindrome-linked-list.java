a04e40bc7c0a0b4f9a560fcb41c5fa3f7987366f
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2024-09-25 09:50:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.java
+++ Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.java
@@ -1,87 +1,67 @@
 //{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
 import java.util.*;
 
-class Node
-{
-	int data;
-	Node next;
-	
-	Node(int d)
-	{
-		data = d;
-		next = null;
-	}
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
 }
 
-class Is_LinkedList_Palindrom
-{
-	 Node head;  
-	 Node tail;
-	
-	/* Function to print linked list */
-    void printList(Node head)
-    {
+class Driver_code {
+    static Node insert(Node head, int data) {
+        Node temp = new Node(data);
+        if (head == null) {
+            head = temp;
+            return head;
+        } else {
+            Node t = head;
+            while (t.next != null) {
+                t = t.next;
+            }
+            t.next = temp;
+        }
+        return head;
+    }
+
+    static void printList(Node head) {
         Node temp = head;
-        while (temp != null)
-        {
-           System.out.print(temp.data+" ");
-           temp = temp.next;
-        }  
-        System.out.println();
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
     }
-	
- 
-    /* Inserts a new Node at front of the list. */
-    public void addToTheLast(Node node) 
-	{
 
-		if (head == null) 
-		{
-			head = node;
-			tail = node;
-		} else 
-		{
-		    tail.next = node;
-		    tail = node;
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+
+            Node head = null;
+
+            String str[] = read.readLine().trim().split(" ");
+            int listSize = str.length;
+            for (int i = 0; i < listSize; i++) {
+                head = insert(head, Integer.parseInt(str[i]));
+            }
+            boolean f = new Solution().isPalindrome(head);
+
+            System.out.println(f ? "true" : "false");
+        }
     }
-	
-	public static void main(String args[])
-	{
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		 
-		while(t>0)
-        {
-			int n = sc.nextInt();
-			//int k = sc.nextInt();
-			Is_LinkedList_Palindrom llist = new Is_LinkedList_Palindrom();
-			//int n=Integer.parseInt(br.readLine());
-			int a1=sc.nextInt();
-			Node head= new Node(a1);
-            Node tail = head;
-            for (int i = 1; i < n; i++) 
-			{
-				int a = sc.nextInt(); 
-			    tail.next = new Node(a);
-			    tail = tail.next;
-			}
-			
-			Solution g = new Solution();
-			if(g.isPalindrome(head) == true)
-			    System.out.println(1);
-		    else
-			    System.out.println(0);
-			t--;
-		}
-		
-	}
 }
-
-
-
 
 // } Driver Code Ends
+
+
 
 
 /* Structure of class Node is
@@ -139,4 +119,3 @@ class Solution
         return true;
     }    
 }
-
