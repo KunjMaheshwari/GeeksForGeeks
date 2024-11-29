9ebdf0ef9d3e7f0d7e79ff233954ed8b599e061e
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2024-11-29 16:39:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.java
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.java
@@ -14,6 +14,8 @@ class GFG {
 
             Solution ob = new Solution();
             System.out.println(ob.longestCommonSubstr(S1, S2));
+
+            System.out.println("~");
         }
     }
 }
@@ -23,18 +25,25 @@ class GFG {
 // User function Template for Java
 
 class Solution {
-    public int longestCommonSubstr(String str1, String str2) {
+    public int longestCommonSubstr(String s1, String s2) {
         // code here
-        int maxLength = 0;
+        int n = s1.length();
+        int m = s2.length();
         
-        for(int i=0;i<str1.length();i++){
-            for(int j=i;j<str1.length();j++){
-                String subStr = str1.substring(i, j+1);
-                if(str2.contains(subStr)){
-                    maxLength = Math.max(maxLength, subStr.length());
+        int maxLen = 0;
+        
+        int dp[][] = new int[n+1][m+1];
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1.charAt(i-1) == s2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                    maxLen = Math.max(maxLen, dp[i][j]);
+                }else{
+                    dp[i][j] = 0;
                 }
             }
         }
-        return maxLength;
+        return maxLen;
     }
 }
