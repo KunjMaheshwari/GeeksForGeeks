e6282fcb66523c2336e9c6dad38b3c4d9e80bc00
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2024-12-03 09:43:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Maximum money/maximum-money.java
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String St[] = read.readLine().split(" ");
+            int N = Integer.parseInt(St[0]);
+            int K = Integer.parseInt(St[1]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.maximizeMoney(N,K));
+        
+System.out.println("~");
+}
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int maximizeMoney(int N , int K) {
+        // code here
+        int res = (N+1)/2;
+        return res*K;
+    }
+};
