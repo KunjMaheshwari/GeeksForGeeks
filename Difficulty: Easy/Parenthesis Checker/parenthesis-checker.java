367a56f6cae3a8d331c3c858ebacf67d0760e1fa
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2025-02-21 10:18:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
@@ -1,64 +1,61 @@
 //{ Driver Code Starts
-import java.util.*;
 import java.io.*;
 import java.lang.*;
+import java.util.*;
 
-class Driverclass
-{
-    public static void main(String args[])
-    {
+class Driverclass {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        
-        //Reading total number of testcases
-        int t= sc.nextInt();
-        
-        while(t-- >0)
-        {
-            //reading the string
+
+        // Reading total number of testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            // reading the string
             String st = sc.next();
-            
-            //calling ispar method of Paranthesis class 
-            //and printing "balanced" if it returns true
-            //else printing "not balanced"
-            if(new Solution().ispar(st) == true)
-                System.out.println("balanced");
+
+            // calling ispar method of Paranthesis class
+            // and printing "balanced" if it returns true
+            // else printing "not balanced"
+            if (new Solution().isBalanced(st) == true)
+                System.out.println("true");
             else
-                System.out.println("not balanced");
-        
+                System.out.println("false");
+
+            System.out.println("~");
         }
     }
 }
 // } Driver Code Ends
 
 
-class Solution
-{
-    //Function to check if brackets are balanced or not.
-    static boolean ispar(String x)
-    {
-        // add your code here
-        Stack<Character> s = new Stack<>();
+
+class Solution {
+    static boolean isBalanced(String s) {
+        // code here
+        Stack<Character> stack = new Stack<>();
         
-        for(int i=0;i<x.length();i++){
-            char ch = x.charAt(i);
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
             //opening
             if(ch == '(' || ch == '{' || ch == '['){
-                s.push(ch);
+                stack.push(ch);
             }else{
                 //closing
-                if(s.isEmpty()){
+                if(stack.isEmpty()){
                     return false;
-                }if((s.peek() == '(' && ch == ')' || s.peek() == '{' && ch == '}' || s.peek() == '[' && ch == ']')){
-                    s.pop();
+                }if((stack.peek() == '(' && ch == ')' || stack.peek() == '{' && ch == '}' || stack.peek() == '[' && ch == ']')){
+                    stack.pop();
                 }else{
                     return false;
                 }
             }
         }
-        if(s.isEmpty()){
+        if(stack.isEmpty()){
             return true;
         }else{
             return false;
+            
         }
     }
 }
